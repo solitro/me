8813ecfb26ea2f39626fba8921541dc150ade6f6
jsolitr3 <solitro.justin@gmail.com>
2022-11-10 20:54:48 -0500

    Configure react app for deployment to Github pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="{https://justinsolitro.com}">
       <Routes>
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
